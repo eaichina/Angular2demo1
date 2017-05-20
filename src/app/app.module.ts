bd315ac5b14e16ebe3a0bd7f360b9bf84fbbe90e
malaotou <marrator@163.com>
2017-05-20 22:07:38 +0800

    77

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { BottomComponent } from './Components/bottom/bottom.component';
+import { MaincontentComponent } from './Components/maincontent/maincontent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BottomComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule,
